268616fb84555c17ae2448e512476d5dabc0df45
ThePiteroXx <thepremiumxx@wp.pl>
2022-05-12 10:14:22 +0200

    start testing

--- src/components/Chat/__test__/Chat.test.js
+++ src/components/Chat/__test__/Chat.test.js
@@ -1,43 +1,69 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { addDoc, setDoc } from 'firebase/firestore';
 import Chat from '../Chat';
 
-jest.mock('firebase/firestore', () => ({
-  ...jest.requireActual('firebase/firestore'),
-  onSnapshot: (q, callback) =>
-    callback([
-      {
-        data: jest.fn(() => ({
-          from: 'uidMock123',
-          text: 'Hello',
-          createdAt: { toDate: jest.fn() },
-        })),
-        id: 'messID',
-      },
-    ]),
-}));
 jest.mock('hooks/useChatContext', () => ({
   useChatContext: () => ({
     dispatchChat: jest.fn(),
   }),
 }));
 
+jest.mock('firebase/firestore');
+
 jest.mock('hooks/useCollection', () => ({
-  useCollection: () => ({
+  useDocument: () => ({
+    document: {
+      displayName: 'mockUser',
+      isOnline: true,
+      photoURL: 'mockUrl',
+    },
+  }),
+  useCollectionWithSub: () => ({
     documents: [
       {
-        displayName: 'mockUser',
-        isOnline: true,
-        photoURL: 'mockUrl',
+        id: 'mockID',
+        from: 'janek',
+        text: 'Hello mockUser',
+        createdAt: { toDate: jest.fn() },
+      },
+      {
+        id: 'mockID2',
+        from: 'mockUser',
+        text: 'Hello janek',
+        createdAt: { toDate: jest.fn() },
       },
     ],
+    currentState: 'loaded',
   }),
 }));
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
 
+const props = {
+  chat: {
+    user: 'uidUser',
+    idChat: 'idChat',
+  },
+  currentUserUid: '21',
+};
+
 describe('Chat', () => {
-    it('Render <Chat />', () => {
-    render(<Chat chat={{ user: 'uidUser', idChat: 'idCHat' }} currentUserUid="21" />);
+  it('Render <Chat />', () => {
+    render(<Chat {...props} />);
+    expect(screen.getByText(/Hello mockUser/i)).toBeTruthy();
+    expect(screen.getByText(/Hello janek/i)).toBeTruthy();
+  });
+
+  it('should click the button which send messeage and expect to call firestore functions', () => {
+    render(<Chat {...props} />);
+    const sendButtonMessage = screen.getByRole('button', { name: /Send/i });
+    const textareaMessage = screen.getByRole('textbox', { name: '' });
+    const form = screen.getByRole('form', { name: '' });
+    expect(textareaMessage).toBeTruthy();
+    userEvent.type(textareaMessage, 'Hikjkjkj');
+    fireEvent.submit(form);
     screen.debug();
+    expect(addDoc).toHaveBeenCalled();
   });
 });

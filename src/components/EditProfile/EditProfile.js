bd6627b10dcd149a90ee380959500be89c0ad4b6
ThePiteroXx <thepremiumxx@wp.pl>
2022-05-14 18:36:05 +0200

    delete useless div

---
+++ src/components/EditProfile/EditProfile.js
@@ -0,0 +1,38 @@
+import { useAuthContext } from 'hooks/auth/useAuthContext';
+import { Link } from 'react-router-dom';
+
+// styles
+import styles from './EditProfile.module.scss';
+
+// images
+import { ReactComponent as Setting } from 'assets/icons/setting.svg';
+import { ReactComponent as Notifications } from 'assets/icons/notifications.svg';
+import { ReactComponent as UserAddIcon } from 'assets/icons/user-add.svg';
+
+// components
+import Avatar from '../Avatar/Avatar';
+
+const EditProfil = () => {
+  const { user } = useAuthContext();
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.user}>
+        <Avatar src={user.photoURL} />
+        <p>{user.displayName}</p>
+        <Link to="/profile" className={styles['edit-btn']}>
+          <Setting />
+        </Link>
+      </div>
+      <div className={styles.icons}>
+        <Link to="/notifications">
+          <Notifications />
+        </Link>
+        <Link to="/add-user">
+          <UserAddIcon />
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default EditProfil;

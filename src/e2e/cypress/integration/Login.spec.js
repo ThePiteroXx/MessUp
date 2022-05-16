5a4b50981b794d16d0ec86a300e6d4b416027cda
ThePiteroXx <thepremiumxx@wp.pl>
2022-05-16 20:46:30 +0200

    create cypress folder

---
+++ src/e2e/cypress/integration/Login.spec.js
@@ -0,0 +1,5 @@
+describe('Login page', () => {
+  it('should login on account', () => {
+    cy.visit('/login');
+  });
+});
---
+++ src/e2e/cypress/support/commands.js
@@ -0,0 +1,27 @@
+// ***********************************************
+// This example commands.js shows you how to
+// crate various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+//
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })e
+import '@testing-library/cypress/add-commands';
+Cypress.Commands.add('login', (email, password) => {});

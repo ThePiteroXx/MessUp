77ef2dffe80f14515ea65c032a6491f3cc8edd6d
ThePiteroXx <thepremiumxx@wp.pl>
2022-05-18 00:09:41 +0200

    added func which clearing firestore indexdb

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,10 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '@testing-library/cypress/add-commands';
+
+Cypress.on('window:before:load', (win) => {
+  win.indexedDB.deleteDatabase('firebaseLocalStorageDb');
+});
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
